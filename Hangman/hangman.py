35a97b5bdd66645098e92a1d9d3ead72eddae3f1
Masoud Maghsoudi <masoud_maghsoudi@yahoo.com>
2024-05-22 22:36:16 +0330

    Hangman Game (#1)
    * Game completed succesfully
    
    * refactored the code to for modularity
    
    * fixed current directory bug.
    
    * added Readme file

---
+++ Hangman/hangman.py
@@ -0,0 +1,103 @@
+"""
+    This is Hangman Class which generates Hangman games.
+    The word is selected from a dictionary file located in 
+    "words.txt" file in the same directory.
+
+    Author: Masoud Maghsoudi
+    Github: https://github.com/masoud-maghsoudi
+    Email:  masoud_maghsoudi@yahoo.com
+"""
+
+from random import choice
+import os
+
+
+class Hangman:
+    words = []
+    __target_word = []
+    user_word = []
+    __penalty_counter = 0
+
+    def __str__(self) -> str:
+        return "Select a word to be guessed by gamer"
+
+    def __init__(self):
+        """
+        Initiates a Hangman game
+        """
+        with open(os.path.join(os.path.dirname(__file__),"words.txt"), "r", encoding="UTF-8") as file:
+            for line in file:
+                self.words.append(line.strip().lower())
+
+    def new_game(self):
+        """Select a random word for target_words from the word list"""
+        self.__target_word = choice(list(self.words))
+        self.user_word.clear()
+        for ch in self.__target_word:
+            self.user_word.append("-")
+        self.__penalty_counter = 0
+
+    def check_guess(self, char: str) -> bool:
+        """
+        checks whether guessed character is in the target word or not
+
+        Args:
+            char (str): Guessed character
+
+        Returns:
+            bool: return True if guessed character is in target_word
+        """
+        return char in self.__target_word
+
+    def update_game_status(self, char: str):
+        """update user_word according user guess
+
+        Args:
+            char (str): user guessed character
+        """
+        if self.check_guess(char):
+            for i in range(len(self.__target_word)):
+                if char == self.__target_word[i]:
+                    self.user_word[i] = char
+        else:
+            self.__penalty_counter += 1
+
+    def accomplished(self) -> bool:
+        """Checks whether user accomplished in the game or not
+
+        Returns:
+            bool: returns True if user successfully accomplished the game
+        """
+        return self.user_word == list(self.__target_word)
+
+    def hanged(self):
+        """Checks whether user lost the game or not
+
+        Returns:
+            bool: returns True if user lost the game
+        """
+        return self.__penalty_counter >= len(self.__target_word)
+
+    def endgame_check(self):
+        """Checks whether the game is ended or not
+
+        Returns:
+            bool: returns True if user successfully accomplished the game
+        """
+        if self.accomplished():
+            print(
+                f'Congratulatinos, Your are the Winner!, The word is "{self.__target_word}"',
+                end="\n \n",
+            )
+        if self.hanged():
+            print(
+                f'Sorry, You Lost!. The word was "{self.__target_word}", maybe next time!',
+                end="\n \n",
+            )
+    def guess_left_count(self) -> int:
+        """returns number of opportunities left to guess
+
+        Returns:
+            int: number of opportunities left to guess
+        """
+        return len(self.__target_word)-self.__penalty_counter
---
+++ Hangman/main.py
@@ -0,0 +1,47 @@
+"""
+    In this script I have written a Hangman Game with OOP.
+
+    Author: Masoud Maghsoudi
+    Github: https://github.com/masoud-maghsoudi
+    Email:  masoud_maghsoudi@yahoo.com
+"""
+
+import hangman as hm
+import os
+
+
+def main():
+    os.chdir(os.path.dirname(__file__))
+    play = True
+    game = hm.Hangman()
+    game.new_game()
+
+    while play:
+        print(
+            f"you have {game.guess_left_count()} opportubities left to guess",
+            end="\n \n",
+        )
+        print(" ".join(game.user_word), end="\n \n")
+        try:
+            user_guess = input("Input your guess: ").lstrip().lower()[0]
+        except IndexError:
+            print("Your input is invalid, please try again")
+            continue
+        game.update_game_status(user_guess)
+        game.endgame_check()
+        if game.accomplished() or game.hanged():
+            try:
+                replay = input("Would you like to replay: ").lstrip().lower()[0]
+            except IndexError:
+                print("Goodbye, Have Fun!")
+                play = False
+                break
+            if replay == "y":
+                game.new_game()
+            else:
+                play = False
+                print("Goodbye, Have Fun!")
+
+
+if __name__ == "__main__":
+    main()
