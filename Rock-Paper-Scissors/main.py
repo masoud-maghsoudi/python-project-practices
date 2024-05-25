118ea4301a5c95d49d39c2839eccc56b8b3bcf36
Masoud Maghsoudi <masoud_maghsoudi@yahoo.com>
2024-05-25 08:33:37 +0330

    added reset button

--- Rock-Paper-Scissors/main.py
+++ Rock-Paper-Scissors/main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import random
-from tkinter import font
 
 
 def check_winner(user_action: str) -> None:
@@ -30,12 +29,18 @@ def check_winner(user_action: str) -> None:
 
     round_no += 1
     lbl_round_count.config(text=f"Number of Played Rounds: {round_no}")
-    
-# def reset_game():
-    # global round_no, scrores
-    # round_no = 0
-    # for keys, items
-    
+
+
+def reset():
+    global round_no
+    round_no = 0
+    for i in scrores.keys():
+        scrores[i] = 0
+    lbl_score_player.config(text=f"Player: {scrores["player"]}")
+    lbl_score_machine.config(text=f"Computer: {scrores["machine"]}")
+    lbl_score_draw.config(text=f"Draw: {scrores["draw"]}")
+    lbl_round_count.config(text=f"Number of Played Rounds: {round_no}")
+    lbl_round_result.config(text="The Game has been reset")
 
 
 actions = ["rock", "paper", "scissors"]
@@ -45,13 +50,13 @@ round_no = 0
 window = tk.Tk()
 window.title("Rock Paper Scissor Game")
 window.rowconfigure(0, minsize=100)
-window.resizable(0,0)
+window.resizable(0, 0)
 
 frm_score = tk.Frame(window, bg="cyan", borderwidth=2, relief="solid")
 frm_round_count = tk.Frame(window, bg="orange", borderwidth=2, relief="solid")
 frm_round_result = tk.Frame(window)
 frm_action = tk.Frame(window)
-frm_action = tk.Frame(window)
+frm_control = tk.Frame(window)
 
 lbl_score_player = tk.Label(
     frm_score, text=f"Player: {scrores["player"]}", font="arial 15", bg="cyan"
@@ -103,24 +108,36 @@ btn_scissors = tk.Button(
     bg="light blue",
     font=15,
 )
+btn_reset = tk.Button(
+    frm_control,
+    text="Reset",
+    command=reset,
+    fg="white",
+    bg="black",
+    font="normal 15 bold",
+    borderwidth=2,
+)
+
 btn_exit = tk.Button(
-    frm_action,
+    frm_control,
     text="EXIT",
     command=lambda: window.destroy(),
     fg="white",
     bg="black",
-    font="normal 20 bold",
+    font="normal 15 bold",
     borderwidth=2,
 )
 
 btn_rock.grid(row=0, column=0, padx=10, pady=5)
 btn_paper.grid(row=0, column=1, padx=10, pady=5)
 btn_scissors.grid(row=0, column=2, padx=10, pady=5)
-btn_exit.grid(row=1, column=1, pady=10)
+btn_reset.grid(row=0, column=0, padx=10, pady=10)
+btn_exit.grid(row=0, column=1, padx=10, pady=10)
 
 frm_score.grid(row=0, padx=10, pady=10)
 frm_round_count.grid(row=1, padx=10, pady=10)
 frm_round_result.grid(row=2, padx=10, pady=10)
 frm_action.grid(row=3, padx=10, pady=10)
+frm_control.grid(row=4, padx=10, pady=10)
 
 window.mainloop()
